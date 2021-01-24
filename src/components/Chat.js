21823290b4f0b5cd8d54f5c268fb2041e04dc14a
StevenPss <pulessteven@gmail.com>
2021-01-24 15:20:24 +0200

    feat: added emojis

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -4,13 +4,26 @@ import { selectUser } from '../features/userSlice';
 import { selectChannelId, selectChannelName } from '../features/appSlice';
 import db from '../firebase';
 import firebase from 'firebase';
+import 'emoji-mart/css/emoji-mart.css'
+import { Picker, emojiIndex } from 'emoji-mart'
+import ReactTextareaAutocomplete from "@webscopeio/react-textarea-autocomplete"
 
 function Chat() {
     const user = useSelector(selectUser);
     const channelId = useSelector(selectChannelId);
     const channelName = useSelector(selectChannelName);
     const [input, setInput] = useState("");
-  
+    const [isEmojiOpen, setIsEmojiOpen] = useState(false)
+
+    const showEmoji = () => {
+      setIsEmojiOpen(!isEmojiOpen)
+    }
+
+    const addEmoji = (emoji) => {
+      const text = `${input}${emoji.native}`;
+      setInput(text)
+    }
+
     const sendMessage = (e) => {
       e.preventDefault();
 
@@ -23,6 +36,14 @@ function Chat() {
       setInput('')
     }
 
+    const handleKeyPress = (e) => {
+      if (e.key === 'Enter') {
+        e.preventDefault();
+        sendMessage(e);
+      }
+    }
+
+
     return (
         <div class="bg-gray-700 h-24 flex items-center mx-3">
             <button class="px-2 py-2 h-10 bg-gray-800 rounded-l text-gray-500 hover:text-white">
@@ -31,8 +52,34 @@ function Chat() {
 
             <div class="flex-1">
               <form action="" method="post">
-                <input type="text" class="w-full text-sm h-10 px-2 py-2 bg-gray-800 text-gray-200 focus:outline-none" disabled={!channelId} value={input} onChange={(e) => setInput(e.target.value)} placeholder={`Message #${channelName}`}/>
-                <button type="submit" className="hidden"onClick={sendMessage}>Send Meassge</button>
+                <ReactTextareaAutocomplete
+                  type="text" 
+                  className="my-textarea w-full text-sm h-10 px-2 py-2 bg-gray-800 text-gray-200 focus:outline-none"
+                  loadingComponent={() => <span>Loading</span>}
+                  disabled={!channelId}
+                  value={input}
+                  onKeyPress={handleKeyPress}
+                  onChange={(e) => setInput(e.target.value)}
+                  placeholder={`Message #${channelName}`}
+                  dropdownStyle={{
+                    maxHeight: 400,
+                    overflow: "auto",
+                 }}
+                  trigger={{
+                    ':': {
+                      dataProvider: token =>
+                        emojiIndex.search(token).sort().map(o => ({
+                          colons: o.colons,
+                          native: o.native,
+                        })),
+                      component: ({ entity: { native, colons } }) => (
+                        <div>{`${colons} ${native}`}</div>
+                      ),
+                      output: item => `${item.native}`,
+                    },
+                  }}
+                />
+                <button type="submit" className="hidden" onClick={sendMessage}>Send Meassge</button>
               </form>
             </div>
 
@@ -43,7 +90,14 @@ function Chat() {
                 <button class="h-10 bg-gray-800 rounded-l text-gray-500 hover:text-white ml-3">
                   <svg class="w-8 h-8" fill="currentColor" width="24" height="24" viewBox="0 0 24 24"><defs><path id="a" d="M24 24H0V0h24v24z"></path></defs><clipPath id="b"><use href="#a" overflow="visible"></use></clipPath><path d="M11.5 9H13v6h-1.5zM9 9H6c-.6 0-1 .5-1 1v4c0 .5.4 1 1 1h3c.6 0 1-.5 1-1v-2H8.5v1.5h-2v-3H10V10c0-.5-.4-1-1-1zm10 1.5V9h-4.5v6H16v-2h2v-1.5h-2v-1z" clip-path="url(#b)"></path></svg>
                 </button>
-                <button class="h-10 bg-gray-800 rounded-l text-gray-500 hover:text-white ml-3">
+
+                {isEmojiOpen? 
+                    <Picker onSelect={addEmoji} style={{ position: 'absolute', bottom: '40px', right: '20px' }} />
+                       :
+                    null
+                }
+
+                <button onClick={showEmoji} class="h-10 bg-gray-800 rounded-l text-gray-500 hover:text-white ml-3">
                   <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 496 512"><path d="M248 8C111 8 0 119 0 256s111 248 248 248 248-111 248-248S385 8 248 8zm80 168c17.7 0 32 14.3 32 32s-14.3 32-32 32-32-14.3-32-32 14.3-32 32-32zm-160 0c17.7 0 32 14.3 32 32s-14.3 32-32 32-32-14.3-32-32 14.3-32 32-32zm194.8 170.2C334.3 380.4 292.5 400 248 400s-86.3-19.6-114.8-53.8c-13.6-16.3 11-36.7 24.6-20.5 22.4 26.9 55.2 42.2 90.2 42.2s67.8-15.4 90.2-42.2c13.4-16.2 38.1 4.2 24.6 20.5z"></path></svg>
                 </button>
             </div>
